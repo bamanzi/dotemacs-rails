585a9d8aa1858192a40b8269d75d58cb856a8cd8
bamanzi <bamanzi@localhost>
2014-03-22 17:56:33 +0800

    add configurations for `projectile-rails'

--- _init-rails.el
+++ _init-rails.el
@@ -1,5 +1,5 @@
-;;* ruby on rails
-;;** rhtml-mode for editing html template
+;; * ruby on rails
+;; ** rhtml-mode for editing html template
 (autoload 'rhtml-mode "rhtml-mode"
   "Embedded Ruby Mode (RHTML)" t)
 
@@ -8,7 +8,7 @@
 ;;(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 
-;;** rinari: rails code navigation & utils
+;; ** rinari: rails code navigation & utils
 ;;http://rinari.rubyforge.org/
 (autoload 'rinari-launch "rinari"
   "Call function `rinari-minor-mode' if inside a rails project." t)
@@ -35,13 +35,31 @@
      (define-key rinari-minor-mode-map (kbd "C-c .") 'robe-jump)
      ))
 
-;;** yaml-mode
+;; ** projectile-rails: a replacement for rinari
+;; https://github.com/asok/projectile-rails
+;; advantages over `rinari':
+;;   + auto uses `zeus' when run 'rails console' and 'rake'
+;;   + `projectile-rails-find-log': open log files with `auto-revert-mode' on
+
+(autoload 'projectile-rails-console "projectile-rails"
+  "Undocumented." t)
+(defalias 'zeus-console 'projectile-rails-console)
+
+(autoload 'projectile-rails-rake "projectile-rails"
+  "Undocumented." t)
+(defalias 'zeus-rake    'projectile-rails-rake)
+
+(autoload 'projectile-rails-find-log "projectile-rails"
+  "Open Rails log with auto-revert-mode on." t)
+
+
+;; ** yaml-mode
 (autoload 'yaml-mode  "yaml-mode"
   "Simple mode to edit YAML." t)
 
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
-;;** anything-rails
+;; ** anything-rails
 (defun anything-rails-files ()
   "Find file in rails project."
   (interactive)
