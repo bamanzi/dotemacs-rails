7a3ae61e1ddea84c25633c4b0e038cea70083ec0
bamanzi <bamanzi@localhost>
2014-05-08 21:52:18 +0800

    make inf-ruby work on emacs 23 (fix half of issue #4)

--- _init-ruby.el
+++ _init-ruby.el
@@ -1,5 +1,5 @@
-;;* ruby
-;;** major mode
+;; * ruby
+;; ** major mode
 (autoload 'enh-ruby-mode  "enh-ruby-mode"
   "Enhanced Major mode for editing Ruby code." t)
 
@@ -15,7 +15,7 @@
      (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
      ))
 
-;;** code folding
+;; ** code folding
 (defun ruby-mode-init-folding ()
   (interactive)
 
@@ -52,7 +52,7 @@
      ))
 
 
-;;** inf-ruby
+;; ** inf-ruby
 (autoload 'run-ruby "inf-ruby"
   "Run an inferior Ruby process")
 (autoload 'inf-ruby-keys "inf-ruby"
@@ -73,8 +73,54 @@
 
 (add-hook 'ruby-mode-hook 'ruby-mode-init-inf-ruby)
 
+(eval-after-load "inf-ruby"
+  `(if (< emacs-major-version 24)
+       ;; backport from emacs-24.3
+       (defun locate-dominating-file (file name)
+         "Look up the directory hierarchy from FILE for a directory containing NAME.
+Stop at the first parent directory containing a file NAME,
+and return the directory.  Return nil if not found.
+Instead of a string, NAME can also be a predicate taking one argument
+\(a directory) and returning a non-nil value if that directory is the one for
+which we're looking."
+         ;; We used to use the above locate-dominating-files code, but the
+         ;; directory-files call is very costly, so we're much better off doing
+         ;; multiple calls using the code in here.
+         ;;
+         ;; Represent /home/luser/foo as ~/foo so that we don't try to look for
+         ;; `name' in /home or in /.
+         (setq file (abbreviate-file-name file))
+         (let ((root nil)
+               ;; `user' is not initialized outside the loop because
+               ;; `file' may not exist, so we may have to walk up part of the
+               ;; hierarchy before we find the "initial UID".  Note: currently unused
+               ;; (user nil)
+               try)
+           (while (not (or root
+                           (null file)
+                           ;; FIXME: Disabled this heuristic because it is sometimes
+                           ;; inappropriate.
+                           ;; As a heuristic, we stop looking up the hierarchy of
+                           ;; directories as soon as we find a directory belonging
+                           ;; to another user.  This should save us from looking in
+                           ;; things like /net and /afs.  This assumes that all the
+                           ;; files inside a project belong to the same user.
+                           ;; (let ((prev-user user))
+                           ;;   (setq user (nth 2 (file-attributes file)))
+                           ;;   (and prev-user (not (equal user prev-user))))
+                           (string-match locate-dominating-stop-dir-regexp file)))
+             (setq try (if (stringp name)
+                           (file-exists-p (expand-file-name name file))
+                         (funcall name file)))
+             (cond (try (setq root file))
+                   ((equal file (setq file (file-name-directory
+                                            (directory-file-name file))))
+                    (setq file nil))))
+           (if root (file-name-as-directory root))))
+   ))
 
-;;** robe: Code navigation, documentation lookup and completion for Ruby
+
+;; ** robe: Code navigation, documentation lookup and completion for Ruby
 
 (autoload 'robe-mode "robe"
   "Improved navigation for Ruby" t)
@@ -82,7 +128,7 @@
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-to-list 'ruby-mode-hook 'turn-on-eldoc-mode)
 
-;;*** enable auto-complete
+;; *** enable auto-complete
 (defun ruby-mode-enable-ac ()
   (interactive)
   (add-to-list 'ac-sources 'ac-source-robe))
@@ -102,7 +148,7 @@
        (message "By default `ac-source-robe' disabled in ruby-mode, call `ruby-mode-enable-ac' to enable it.")
        )))
 
-;;*** zossima: a lightweight robe
+;; *** zossima: a lightweight robe
 ;;(actually, robe was forked from zossima)
 (autoload 'zossima-mode "zossima"
   "Improved navigation for Ruby" t)
@@ -110,7 +156,7 @@
 
 
 
-;;** rdebug
+;; ** rdebug
 ;;for ruby > 1.9, install 'debugger' gem
 ;;
 ;;(add-to-list 'load-path "/usr/lib/ruby/gems/1.9.1/debugger-1.6.0/emacs")
@@ -124,23 +170,23 @@
       (provide 'gdb-ui))))
 
 
-;;** misc
-;;*** ruby-block
+;; ** misc
+;; *** ruby-block
 (autoload 'ruby-block-mode  "ruby-block"
   "In ruby-mode, Displays the line where there is keyword corresponding" t)
 (setq ruby-block-highlight-toggle t)
 
 (add-hook 'ruby-mode-hook 'ruby-block-mode)
 
-;;*** rake
+;; *** rake
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 
-;;*** ruby-tools: string/symbol conversion
+;; *** ruby-tools: string/symbol conversion
 (eval-after-load "ruby-mode"
   `(progn
      (require 'ruby-tools nil t)))
 
-;;*** speedbar
+;; *** speedbar
 (eval-after-load "speedbar"
   `(progn
      (speedbar-add-supported-extension ".rb")
@@ -149,11 +195,11 @@
      (speedbar-add-supported-extension ".rake")
      ))
 
-;;*** rubocop
+;; *** rubocop
 (autoload 'rubocop-check-current-file "rubocop"
   "Run on current file." t)
 
-;;*** which-func
+;; *** which-func
 (eval-after-load "which-func"
   `(progn
      (add-to-list 'which-func-modes 'ruby-mode)
