5ce6a0bb8d0c90d50706bc755a42ecc44caaebfe
Ba Manzi <bamanzi@gmail.com>
2016-04-29 19:01:46 +0800

    update package ob-http

--- web/ob-http.el
+++ web/ob-http.el
@@ -1,11 +1,24 @@
 ;;; ob-http.el --- http request in org-mode babel
 
-;; Copyright (C) 2015 ZHOU Feng
+;; Copyright (C) 2015 Feng Zhou
 
 ;; Author: ZHOU Feng <zf.pascal@gmail.com>
 ;; URL: http://github.com/zweifisch/ob-http
 ;; Version: 0.0.1
 ;; Package-Requires: ((s "1.9.0") (cl-lib "0.5"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
@@ -16,6 +29,7 @@
 (require 'org)
 (require 'ob)
 (require 's)
+(require 'subr-x)
 (require 'json)
 (require 'ob-http-mode)
 (require 'cl-lib)
@@ -23,12 +37,16 @@
 (defconst org-babel-header-args:http
   '((pretty . :any)
     (proxy . :any)
+    (noproxy . :any)
+    (curl . :any)
     (cookie . :any)
     (schema . :any)
     (host . :any)
     (port . :any)
     (username . :any)
     (password . :any)
+    (follow-redirect . :any)
+    (path-prefix . :any)
     (max-time . :any))
   "http header arguments")
 
@@ -45,6 +63,11 @@
   "remove carriage return from header"
   :group 'ob-http
   :type 'boolean)
+
+(defcustom ob-http:curl-custom-arguments nil
+  "List of custom headers that shall be added to each curl request"
+  :group 'ob-http
+  :type '(repeat (string :format "%v")))
 
 (cl-defstruct ob-http-request method url headers body)
 (cl-defstruct ob-http-response headers body headers-map)
@@ -68,7 +91,11 @@
      :headers-map headers-map)))
 
 (defun ob-http-split-header-body (input)
-  (s-split-up-to "\\(\r\n\\|[\n\r]\\)[ \t]*\\1" input 1))
+  (let ((splited (s-split-up-to "\\(\r\n\\|[\n\r]\\)[ \t]*\\1" input 1)))
+    (if (and (string-match "^HTTP/1.[0-1] \\(30\\|100\\)" (car splited))
+             (string-match "^HTTP/1.[0-1]" (cadr splited)))
+        (ob-http-split-header-body (cadr splited))
+      splited)))
 
 (defun ob-http-parse-header (line)
   (let ((key-value (s-split-up-to ": " line 1)))
@@ -102,6 +129,7 @@
 
 (defun ob-http-pretty-html (str)
   (cond
+   ((executable-find "elinks") (ob-http-shell-command-to-string "elinks -dump" str))
    ((executable-find "tidy") (ob-http-shell-command-to-string "tidy -i -raw -q 2> /dev/null" str))
    ((executable-find "pup") (ob-http-shell-command-to-string "pup -p" str))
    (t str)))
@@ -109,7 +137,7 @@
 (defun ob-http-pretty (body content-type)
   (if (string= "" body)
       body
-    (case (ob-http-parse-content-type content-type)
+    (cl-case (ob-http-parse-content-type content-type)
       (json (ob-http-pretty-json body))
       (xml (ob-http-pretty-xml body))
       (html (ob-http-pretty-html body))
@@ -137,7 +165,7 @@
 
 (defun org-babel-expand-body:http (body params)
   (s-format body 'ob-http-aget
-            (mapcar #'cdr (org-babel-get-header params :var))))
+            (mapcar (lambda (x) (when (eq (car x) :var) (cdr x))) params)))
 
 (defun ob-http-get-response-header (response header)
   (cdr (assoc (s-downcase header) (ob-http-response-headers-map response))))
@@ -166,24 +194,36 @@
        (assoc-default :host params)
        (when (assoc :port params)
              (format ":%s" (assoc-default :port params)))
+       (assoc-default :path-prefix params)
        path)
     path))
+
+(defun ob-http-file (response filename)
+  (let ((body (ob-http-response-body response)))
+    (with-temp-file filename
+      (insert body))))
 
 (defun org-babel-execute:http (body params)
   (let* ((request (ob-http-parse-request (org-babel-expand-body:http body params)))
          (proxy (cdr (assoc :proxy params)))
+         (noproxy (assoc :noproxy params))
+         (follow-redirect (and (assoc :follow-redirect params) (not (string= "no" (cdr (assoc :follow-redirect params))))))
          (pretty (assoc :pretty params))
          (prettify (and pretty (not (string= (cdr pretty) "no"))))
+         (file (assoc :file params))
          (get-header (cdr (assoc :get-header params)))
          (cookie-jar (cdr (assoc :cookie-jar params)))
          (cookie (cdr (assoc :cookie params)))
+         (curl (cdr (assoc :curl params)))
          (select (cdr (assoc :select params)))
          (request-body (ob-http-request-body request))
          (error-output (org-babel-temp-file "curl-error"))
-         (args (list "-i"
-                     (when proxy `("-x" ,proxy))
+         (args (append ob-http:curl-custom-arguments (list "-i"
+                     (when (and proxy (not noproxy)) `("-x" ,proxy))
+                     (when noproxy '("--noproxy" "*"))
                      (let ((method (ob-http-request-method request)))
                        (if (string= "HEAD" method) "-I" `("-X" ,method)))
+                     (when follow-redirect "-L")
                      (mapcar (lambda (x) `("-H" ,x)) (ob-http-request-headers request))
                      (when (s-present? request-body)
                        (let ((tmp (org-babel-temp-file "http-")))
@@ -191,10 +231,15 @@
                          `("-d" ,(format "@%s" tmp))))
                      (when cookie-jar `("--cookie-jar" ,cookie-jar))
                      (when cookie `("--cookie" ,cookie))
+                     (when curl (split-string-and-unquote curl))
                      "--max-time"
                      (int-to-string (or (cdr (assoc :max-time params))
                                         ob-http:max-time))
-                     (ob-http-construct-url (ob-http-request-url request) params))))
+                     (ob-http-construct-url (ob-http-request-url request) params)))))
+    (with-current-buffer (get-buffer-create "*curl commands history*")
+      (goto-char (point-max))
+      (let ((cmd-line (concat "curl " (string-join (ob-http-flatten args) " ") "\n")))
+        (insert (replace-regexp-in-string "--noproxy \\*"  "--noproxy '*'" cmd-line))))
     (with-current-buffer (get-buffer-create "*curl output*")
       (erase-buffer)
       (if (= 0 (apply 'call-process "curl" nil `(t ,error-output) nil (ob-http-flatten args)))
@@ -204,8 +249,9 @@
             (cond (get-header (ob-http-get-response-header response get-header))
                   (select (ob-http-select response select))
                   (prettify (ob-http-response-body response))
+                  (file (ob-http-file response (cdr file)))
                   (t (s-join "\n\n" (list (ob-http-response-headers response) (ob-http-response-body response))))))
-        (with-output-to-temp-buffer "*curl error"
+        (with-output-to-temp-buffer "*curl error*"
           (princ (with-temp-buffer
                    (insert-file-contents-literally error-output)
                    (s-join "\n" (s-lines (buffer-string)))))
